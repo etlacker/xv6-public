803ac059e946dc778e19f334932be18016ee45a5
etlacker <31419044+etlacker@users.noreply.github.com>
2019-03-27 19:57:07 -0400

    Create test_getreadcount.c

---
+++ test_getreadcount.c
@@ -0,0 +1,25 @@
+#include "stat.h"
+#include "user.h"
+ 
+int main(void) {
+
+  for (int i = 0; i < 5; i++){
+    printf(1, "i -> %d, West Chester University was founded in %d\n", i, wcupa());
+  }
+  
+  printf(1, "getreadcount() -> %d\n", getreadcount());
+  
+  for (i; i < 10; i++){
+    printf(1, "i -> %d, West Chester University was founded in %d\n", i, wcupa());
+  }
+  
+  printf(1, "getreadcount() -> %d\n", getreadcount());
+  
+  for (i; i < 15; i++){
+    printf(1, "i -> %d, West Chester University was founded in %d\n", i, wcupa());
+  }
+  
+  printf(1, "getreadcount() -> %d\n", getreadcount());
+  
+  exit();
+}
