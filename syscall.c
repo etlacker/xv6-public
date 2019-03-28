3d8adc94463475a76cc8381b5370e0d34e777141
etlacker <31419044+etlacker@users.noreply.github.com>
2019-03-27 20:54:29 -0400

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -105,6 +105,7 @@ extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_wcupa(void);
 extern int sys_getreadcount(void);
+extern int readcount;
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
