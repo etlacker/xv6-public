6f1d844a2e4470af343077aa004d37bde8e69beb
etlacker <31419044+etlacker@users.noreply.github.com>
2019-03-27 21:13:33 -0400

    Update test_getreadcount.c

--- test_getreadcount.c
+++ test_getreadcount.c
@@ -1,6 +1,5 @@
 #include "stat.h"
 #include "user.h"
-#include "types.h"
  
 int main(void) {
  
@@ -9,13 +8,13 @@ int main(void) {
   printf(1, "getreadcount() -> %d\n", getreadcount());
   
   for (int i = 1; i < 4; i++){
-    printf(1, "i -> %d, West Chester University was founded in %d\n", i, wcupa());
+    printf(1, "i -> %d, wcupa() -> %d\n", i, wcupa());
   }
   
   printf(1, "getreadcount() -> %d\n", getreadcount());
   
   for (int i = 4; i < 6; i++){
-    printf(1, "i -> %d, West Chester University was founded in %d\n", i, wcupa());
+    printf(1, "i -> %d, wcupa() -> %d\n", i, wcupa());
   }
   
   printf(1, "getreadcount() -> %d\n", getreadcount());
